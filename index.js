a047435fc5ddb2090907b3f5b3900e0c138addcd
Maria Jose Parejo <mariajoseparejodesousa@gmail.com>
2023-07-04 19:00:02 -0400

    funcion de renderizado JS

--- index.js
+++ index.js
@@ -1,5 +1,15 @@
 //variables constantes
-const curiosityText = document.querySelector('.curiosity-text')
+
+//cuiosidades
+const curiosityText = document.querySelector('.curiosity-text');
+
+//portafolio
+const portAcademico = document.querySelector('.academico');
+const portProfesional = document.querySelector('.profesional');
+const portDocente = document.querySelector('.docente');
+const portTotal = document.querySelector('.port-total');
+
+
 
 //addEventListener
 
@@ -22,20 +32,169 @@ let curiosities = [
     'Un topo puede cavar un túnel de 300 pies de largo en solo una noche.'
 ]
 let curiosityPrint
+let academicoList = [];
+let profesionalList = [];
+let docenteList = [];
+let totalList = [];
 
 
 //functios
 
 function random (min, arr) {
-    return Math.floor(Math.random() * (arr.length - min))
+    return Math.floor(Math.random() * (arr.length - min));
 }
 
 function curiosityGenerator (arr) {
-   let index = random(0, arr)
-   curiosityPrint = arr[index]
+   let index = random(0, arr);
+   curiosityPrint = arr[index];
 
-   curiosityText.innerText = (curiosityPrint)
-   console.log(index)
+   curiosityText.innerText = (curiosityPrint);
 }
 
-curiosityGenerator(curiosities)
+curiosityGenerator(curiosities);
+
+
+//section de portafolio
+
+function Projects (category, name, date, description, portada, plans, album) {
+    this.category = category;
+    this.name = name;
+    this.date = date;
+    this.description = description;
+    this.portada = portada;
+    this.plans = plans;
+    this.album = album;
+}
+
+let posada = new Projects (
+    'academico',
+    'Posada En Choroni',
+    2013,
+    'Proyecto realizado en el 3er semestre de la carrera. La posada en Choroní busca reinterpretar la arquitectura colonial y convertirla en arquitectura contemporánea.',
+    '/portafolio/posada/posada1.jpg',
+    [
+        '/portafolio/posada/planta-pb.png',
+        '/portafolio/posada/planta-p1.png'
+    ],
+    [
+        '/portafolio/posada/posada1.jpg',
+        '/portafolio/posada/posada2.jpg',
+        '/portafolio/posada/posada3.jpg',
+        '/portafolio/posada/posada4.jpg'
+    ],
+)
+
+let spa = new Projects (
+    'academico',
+    'Centro de salud y Spa',
+    2014,
+    'Proyecto realizado en el 4to semestre de la carrera. Este proyecto, además de exigir el desarrollo de una edificación, también exigía trabajo topográfico. El centro de salud, se desarrolla como una edificación que se impone en la parte alta de la colina, donde con juego de patios y terrazas busca lograr distintas sensaciones espaciales.',
+    '/portafolio/spa/SPA_VISTA2.jpg',
+    [
+    ],
+    [
+        '/portafolio/spa/SPA_VISTA1.jpg',
+        '/portafolio/spa/SPA_VISTA2.jpg',
+        '/portafolio/spa/SPA_VISTA3.jpg',
+        '/portafolio/spa/SPA_VISTA4.jpg'
+    ],
+)
+
+let hotel = new Projects (
+    'academico',
+    'Hotel Boutique Spa',
+    2016,
+    'Proyecto realizado en el 6to semestre de la carrera.Tenía como objetivo la apropiación de un terreno ficticio de pendiente perfecta rodeado de árboles. La creación del lugar, se realiza a través de una trama espacial que organízalos volúmenes que forman el conjunto.',
+    '/portafolio/hotel/HOTEL_2.jpg',
+    [
+    ],
+    [
+        '/portafolio/spa/SPA_VISTA1.jpg',
+        '/portafolio/spa/SPA_VISTA2.jpg',
+        '/portafolio/spa/SPA_VISTA3.jpg',
+        '/portafolio/spa/SPA_VISTA4.jpg'
+    ],
+)
+
+let mercado = new Projects (
+    'academico',
+    'Cubiertas en la Selva Tropical',
+    2018,
+    'Proyecto realizado en el 9no semestre de la carrera. El proyecto busca solucionar la falta de mercados consolidados en el pueblo de Choron¬í, por lo que a través de una cubierta de madera y volúmenes de tiendas busca crear espacios al aire libre que permitan el tamizado de la luz.',
+    '/portafolio/mercado/CUBIERTA_VISTA1.jpg',
+    [
+    ],
+    [
+        '/portafolio/spa/SPA_VISTA1.jpg',
+        '/portafolio/spa/SPA_VISTA2.jpg',
+        '/portafolio/spa/SPA_VISTA3.jpg',
+        '/portafolio/spa/SPA_VISTA4.jpg'
+    ],
+
+)
+
+
+
+academicoList.push(posada, spa, hotel, mercado);
+profesionalList.push(hotel);
+docenteList.push(mercado);
+totalList.push(posada, spa, hotel);
+
+
+function projectListHome() {
+
+    for(let i = 0; i < totalList.length; i++){
+        let works = totalList[i];
+
+        const projectCard = document.createElement('div');
+        projectCard.classList.add('project-card-home');
+
+        const enlacePort = document.createElement('a');
+        enlacePort.setAttribute('href', '/sectionsHTML/portafolio.html');
+
+        const portada = document.createElement('img');
+        portada.setAttribute('src', works.portada);
+
+        portTotal.append(projectCard);
+        projectCard.appendChild(enlacePort);
+        enlacePort.appendChild(portada);
+        
+    }
+
+}
+
+
+function projectList(arr, variable) {
+
+    for(let i = 0; i < arr.length; i++){
+        let works = arr[i];
+
+        const projectCard = document.createElement('div');
+        projectCard.classList.add('project-card');
+
+        const figurePortada = document.createElement('figure');
+    
+        const portada = document.createElement('img');
+        portada.setAttribute('src', works.portada);
+
+        const textContainer = document.createElement('div');
+        textContainer.classList.add('text-container');
+
+        const projectName = document.createElement('p');
+        projectName.innerText = works.name
+
+        const projectdate = document.createElement('p');
+        projectdate.innerText = works.date
+
+        variable.append(projectCard);
+        projectCard.append(figurePortada, textContainer);
+        figurePortada.appendChild(portada);
+        textContainer.append(projectName, projectdate)
+        
+    }
+
+}
+
+projectList(academicoList, portAcademico)
+projectList(profesionalList, portProfesional)
+projectList(docenteList, portDocente)
